ea1e284f54876208c167164b5369f106ec15b264
James Morris <blondfro@gmail.com>
2018-03-16 17:00:53 -0500

    added in an events-app component and referenced it's selector in the html file.  added the angular module and bootstrapped the module.

---
+++ ng2-fundamentals/app/events-app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: '<h2>Hello World</h2>'
+})
+
+export class EventsAppComponent {
+}
+
+
+
+/**
+ * Created by me on 3/16/2018.
+ */
---
+++ ng2-fundamentals/app/main.ts
@@ -0,0 +1,11 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
+
+/**
+ * Created by me on 3/16/2018.
+ */
