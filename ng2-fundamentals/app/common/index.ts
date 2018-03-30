3261d47669a9ecfb29e1c2ff41cf073f7cdb8d2c
James Morris <blondfro@gmail.com>
2018-03-30 15:08:57 -0500

    created a new jQuery service to be able to make use of bootstrap modals.
    created an index file for the common directory. modified the app module to make use of the new index file and the jQuery service.

---
+++ ng2-fundamentals/app/common/index.ts
@@ -0,0 +1,8 @@
+export * from './collapisible-well.component';
+export * from './toastr.service';
+export * from './jQuery.service';
+
+
+/**
+ * Created by me on 3/30/2018.
+ */
---
+++ ng2-fundamentals/app/common/jQuery.service.ts
@@ -0,0 +1,8 @@
+import { OpaqueToken } from "@angular/core";
+
+export let JQ_TOKEN = new OpaqueToken('jQuery');
+
+
+/**
+ * Created by me on 3/30/2018.
+ */
