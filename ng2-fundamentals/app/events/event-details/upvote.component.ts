1898944a71e2cf8b35dcc7b1ae10b1ffb9d3db90
James Morris <blondfro@gmail.com>
2018-03-30 22:53:45 -0500

    created an input setter on the voted input and gave it a function that would change the color of heart from white to red depending on if the current user has voted for a session.

--- ng2-fundamentals/app/events/event-details/upvote.component.ts
+++ ng2-fundamentals/app/events/event-details/upvote.component.ts
@@ -7,8 +7,8 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
         <div class="votingWidgetContainer pointable" (click)="onClick()">
             <div class="well votingWidget">
                 <div>
-                    <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-                    <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+                    <i class="glyphicon glyphicon-heart" 
+                       [style.color]="iconColor"></i>
                 </div>
             <div>
                 <div class="badge badge-inverse votingCount">
@@ -22,8 +22,12 @@ import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 export class UpvoteComponennt {
     @Input() count: number;
-    @Input() voted: boolean;
+    @Input() set voted(val) {
+        this.iconColor = val ? 'red' : 'white';
+    };
     @Output() vote = new EventEmitter();
+    iconColor: string;
+
 
     onClick() {
         this.vote.emit({});
